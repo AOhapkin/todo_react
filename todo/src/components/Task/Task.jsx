e158082a5de4bfd12b270671cfac9d256b01f0f4
Alexei Okhapkin <alexeyyago@gmail.com>
2024-03-12 11:02:55 +0300

    добавил редактирование и таймер к задаче

--- todo/src/components/Task/Task.jsx
+++ todo/src/components/Task/Task.jsx
@@ -4,47 +4,103 @@ import PropTypes, { func } from 'prop-types';
 
 import './Task.css';
 
-export default function Task({ onToggleDone, deleteTodoItem, todo }) {
-  const { description, id, active, created } = todo;
-  let currentClassName = 'active';
+export default function Task({
+  description,
+  onEditingSubmit,
+  remainingTime,
+  created,
+  toggleComplete,
+  done,
+  onDeleted,
+  toEditing,
+  startTimer,
+  stopTimer,
+}) {
+  const [editingValue, setEditingValue] = useState(description)
 
-  if (!active) {
-    currentClassName = 'completed';
+  const formatMinutes = (time) => {
+    const minutes = Math.floor(time / 60000)
+    return minutes < 10 ? `0${minutes}` : minutes
+  }
+  const formatSeconds = (time) => {
+    const seconds = Math.floor((time % 60000) / 1000)
+    return seconds < 10 ? `0${seconds}` : seconds
+  }
+  const onEditingLabel = (evt) => {
+    setEditingValue(evt.target.value)
   }
 
+  const onSubmit = (evt) => {
+    evt.preventDefault()
+    onEditingSubmit(editingValue)
+  }
+
+  const minutes = formatMinutes(remainingTime)
+  const seconds = formatSeconds(remainingTime)
+  let descriptionClassNames = 'description'
+  if (done) {
+    descriptionClassNames += ' done'
+  } else {
+    descriptionClassNames = 'description'
+  }
+
+  const timeInWords = formatDistanceToNow(created, { includeSeconds: true })
+
   return (
-    <li className={currentClassName}>
+    <>
       <div className="view">
-        <input
-          id={id}
-          className="toggle"
-          type="checkbox"
-          checked={!active}
-          onChange={() => onToggleDone(id)}
-        />
-        <label>
-          <span className="description">{description}</span>
-          <span className="created">
-            {formatDistanceToNow(created, { includeSeconds: true })}
+        <input type="checkbox" checked={done} className="toggle" onChange={toggleComplete} />
+        <label htmlFor="description">
+          <span
+            role="button"
+            tabIndex={0}
+            className={descriptionClassNames}
+            onClick={toggleComplete}
+            onKeyDown={(event) => (event.key === 'Enter' ? toggleComplete() : null)}
+          >
+            {description}
           </span>
+          <span className="description timer">
+            <button type="button" className="icon icon-play" aria-label="Play" onClick={startTimer} />
+            <button type="button" className="icon icon-pause" aria-label="Pause" onClick={stopTimer} />
+            {minutes}:{seconds}
+          </span>
+          <span className="created">Created {timeInWords} ago</span>
         </label>
-        <button className="icon icon-destroy" onClick={() => deleteTodoItem(id)}></button>
+        <button type="button" className="icon icon-edit" onClick={toEditing} aria-label="edit" />
+        <button type="button" className="icon icon-destroy" onClick={onDeleted} aria-label="destroy" />
       </div>
-    </li>
-  );
+      <input
+        type="text"
+        className="edit"
+        value={editingValue}
+        onKeyDown={(event) => (event.key === 'Enter' ? onSubmit(event) : null)}
+        onChange={onEditingLabel}
+      />
+    </>
+  )
 }
 
 Task.propTypes = {
-  onToggleDone: PropTypes.func.isRequired,
-  deleteTodoItem: PropTypes.func.isRequired,
-  todo: PropTypes.shape({
-    description: PropTypes.string,
-    id: PropTypes.number,
-    active: PropTypes.bool,
-    created: PropTypes.instanceOf(Date),
-  }),
+  description: PropTypes.string,
+  toggleComplete: PropTypes.func.isRequired,
+  done: PropTypes.bool,
+  onDeleted: PropTypes.func.isRequired,
+  toEditing: PropTypes.func,
+  onEditingSubmit: PropTypes.func,
+  created: PropTypes.number,
+  remainingTime: PropTypes.number,
+  startTimer: PropTypes.func,
+  stopTimer: PropTypes.func,
 };
 
 Task.defaultProps = {
-  todo: {},
+  description: '',
+  toEditing: () => {},
+  onEditingSubmit: () => {},
+  done: false,
+  created: Date.now(),
+  startTimer: () => {},
+  stopTimer: () => {},
+  remainingTime: 0,
 };
